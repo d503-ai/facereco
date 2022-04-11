51dd9f69d9f30a8bea5786a0de0dc891cbee02e7
d503ai <thelesnikgood@gmail.com>
2022-04-11 09:29:57 +0300

    django learning

--- diploma/django_test/mytestsite/websitre/views.py
+++ diploma/django_test/mytestsite/websitre/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseNotFound
 
 def index(request):
     return HttpResponse("The main page")
 
-def about(request):
-    return HttpResponse("About page")
+def about(request, webid):
+    if webid:
+        print(webid)
+    else:
+        return redirect('home', permanent=True)
+    return HttpResponse(f"About page<p>{webid}</p>")
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("Page not found...")
